16de45e211621a985cd33779c58baa813ae2496d
daniel muniz <munidaniel@gmail.com>
2025-12-01 19:12:41 -0300

    implemented publisher

---
+++ publisher.py
@@ -0,0 +1,37 @@
+import pika
+import json
+
+class RabbitMQPublisher:
+    def __init__(self) -> None:
+        self.__host = "localhost"
+        self.__port = 5672
+        self.__username = "admin"
+        self.__password = "admin"
+        self.__exchange = "minha_exchange"
+        self.__routing_key = ""
+        self.__channel = self.create_channel()
+
+    def create_channel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host=self.__host,
+            port=self.__port,
+            credentials=pika.PlainCredentials(
+                username=self.__username,
+                password=self.__password
+            )
+        )
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        return channel
+
+    def send_message(self, body: dict):
+        self.__channel.basic_publish(
+            exchange=self.__exchange,
+            routing_key=self.__routing_key,
+            body=json.dumps(body),
+            properties=pika.BasicProperties(
+                delivery_mode=2
+            )
+        )
+    
+rabbitMQPublisher = RabbitMQPublisher()
+rabbitMQPublisher.send_message({"message": "Hello, RabbitMQ!"})
